5e0aa407419ccb4eb70d24d905262b8816fbcfac
Roselyne <roselyne.lavalley@gmail.com>
2015-03-09 09:17:11 +0100

    Created TestDataManager

---
+++ DataManagers/TestDataManager.php
@@ -0,0 +1,41 @@
+<?
+use \Framework\Newnorth\Application;
+use \Framework\Newnorth\DataManager;
+use \Framework\Newnorth\DbSelectQuery;
+use \Framework\Newnorth\DbAnd;
+use \Framework\Newnorth\DbEqualTo;	
+
+class TestDataManager extends DataManager {
+	/* Magic methods */
+
+	public function __construct() {
+		$this->Connection = Application::GetDbConnection('Default');
+	}
+
+	/* Methods */
+
+	public function FindById($Id) {
+		$Query = new DbSelectQuery();
+
+		$Query->AddSource('Test');
+
+		$Query->Conditions = new DbEqualTo('`Id`', (int)$Id);
+
+		return $this->_Find($Query);
+	}
+
+	public function FindAllByState($State, $SortColumn = '`TimeFailed`', $SortOrder = DB_DESC) {
+		$Query = new DbSelectQuery();
+
+		$Query->AddSource('Test');
+
+		$Query->Conditions = new DbAnd();
+
+		$Query->Conditions->EqualTo('`State`', '"'.$State.'"');
+
+		$Query->AddSort($SortColumn, $SortOrder);
+
+		return $this->_FindAll($Query);
+	}
+}
+?>
