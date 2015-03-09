5c9c9d691b6a857b0754c6f6c1d56faafe1bb65b
Roselyne <roselyne.lavalley@gmail.com>
2015-03-09 10:11:06 +0100

    Created ScheduledTaskDataManager

---
+++ DataManagers/ScheduledTaskDataManager.php
@@ -0,0 +1,56 @@
+<?
+use \Framework\Newnorth\Application;
+use \Framework\Newnorth\DataManager;
+use \Framework\Newnorth\DbSelectQuery;
+use \Framework\Newnorth\DbAnd;
+use \Framework\Newnorth\DbEqualTo;	
+use \Framework\Newnorth\DbGreaterThan;	
+
+class ScheduledTaskDataManager extends DataManager {
+	/* Magic methods */
+
+	public function __construct() {
+		$this->Connection = Application::GetDbConnection('Default');
+	}
+
+	/* Methods */
+
+	public function FindById($Id) {
+		$Query = new DbSelectQuery();
+
+		$Query->AddSource('ScheduledTask');
+
+		$Query->Conditions = new DbEqualTo('`Id`', (int)$Id);
+
+		return $this->_Find($Query);
+	}
+
+	public function FindAllExecuted($SortColumn = '`PriorityLevel`', $SortOrder = DB_DESC) {
+		$Query = new DbSelectQuery();
+
+		$Query->AddSource('ScheduledTask');
+
+		$Query->Conditions = new DbAnd();
+
+		$Query->Conditions->GreaterThan('`TimeExecuted`', 0);
+
+		$Query->AddSort($SortColumn, $SortOrder);
+
+		return $this->_FindAll($Query);
+	}
+
+	public function FindAllToExecute($SortColumn = '`PriorityLevel`', $SortOrder = DB_DESC) {
+		$Query = new DbSelectQuery();
+
+		$Query->AddSource('ScheduledTask');
+
+		$Query->Conditions = new DbAnd();
+
+		$Query->Conditions->EqualTo('`TimeExecuted`', 0);
+
+		$Query->AddSort($SortColumn, $SortOrder);
+
+		return $this->_FindAll($Query);
+	}
+}
+?>
