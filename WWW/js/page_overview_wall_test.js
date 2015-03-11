017e5595e0ef047abce264504463250e071df217
Roselyne <roselyne.lavalley@gmail.com>
2015-03-11 15:28:41 +0100

    Implemented "Next execution" to tests in "Wall"

--- WWW/js/page_overview_wall_test.js
+++ WWW/js/page_overview_wall_test.js
@@ -11,7 +11,9 @@ Overview.Wall.Test = function(id, test) {
 
 	this.DescriptionElement = this.Element.childNodes[1].childNodes[1];
 
-	this.TimeElapsedElement = this.Element.childNodes[2].childNodes[1];
+	this.NextExecutionElement = this.Element.childNodes[2].childNodes[1];
+
+	this.TimeElapsedElement = this.Element.childNodes[3].childNodes[1];
 }
 
 Overview.Wall.Test.LoadHtml = function() {
@@ -26,16 +28,30 @@ Overview.Wall.Test.LoadHtml = function() {
 
 Overview.Wall.Test.prototype.Update = function(time) {
 	this.UpdateTimeElapsed(time);
+
+	this.UpdateNextExecution(time);
 }
 
 Overview.Wall.Test.prototype.UpdateTimeElapsed = function(time) {
-	var timeElapsed = time - this.Test.TimeLastFailed;
+	var timeElapsed = Math.floor(time - this.Test.TimeLastFailed);
 
-	var seconds = Math.floor(timeElapsed) % 60;
+	var seconds = timeElapsed % 60;
 
 	var minutes = Math.floor(timeElapsed / 60) % 60;
 
 	var hours = Math.floor(timeElapsed / 3600);
 
 	this.TimeElapsedElement.innerHTML = (9 < hours ? hours : "0" + hours) + ":" + (9 < minutes ? minutes : "0" + minutes) + ":" + (9 < seconds ? seconds : "0" + seconds);
+}
+
+Overview.Wall.Test.prototype.UpdateNextExecution = function(time) {
+	var nextExecution = Math.ceil(this.Test.TimeLastExecuted + this.Test.ExecutionInterval - time);
+
+	var seconds = nextExecution % 60;
+
+	var minutes = Math.floor(nextExecution / 60) % 60;
+
+	var hours = Math.floor(nextExecution / 3600);
+
+	this.NextExecutionElement.innerHTML = (9 < hours ? hours : "0" + hours) + ":" + (9 < minutes ? minutes : "0" + minutes) + ":" + (9 < seconds ? seconds : "0" + seconds);
 }
