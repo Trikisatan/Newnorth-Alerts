5e003419879254c9f42bf78de6e315457e2a3b8e
Roselyne <roselyne.lavalley@gmail.com>
2015-04-16 15:41:59 +0200

    Created TestFailureStateDataManager

--- WWW/DataManagers/TestFailureStateDataManager.php
+++ WWW/DataManagers/TestFailureStateDataManager.php
@@ -1,5 +1,5 @@
 <?
-class TestFailureDataManager extends \Framework\Newnorth\DataManager {
+class TestFailureStateDataManager extends \Framework\Newnorth\DataManager {
 	/* Magic methods */
 
 	public function __construct() {
@@ -8,36 +8,16 @@ class TestFailureDataManager extends \Framework\Newnorth\DataManager {
 
 	/* Methods */
 
-	public function Insert($TestId) {
-		$Query = new \Framework\Newnorth\DbInsertQuery();
-
-		$Query->Source = 'TestFailure';
-
-		$Query->AddColumn('`TestId`');
-		$Query->AddColumn('`TimeFailed`');
-
-		$Query->AddValue($TestId);
-		$Query->AddValue(time());
-
-		$Id = $this->_Insert($Query);
-
-		if($Id === false) {
-			return null;
-		}
-
-		return $this->FindById($Id);
-	}
-
 	public function FindById($Id) {
 		$Query = new \Framework\Newnorth\DbSelectQuery();
 
-		$Query->AddSource('TestFailure');
+		$Query->AddSource('TestFailure-State');
 
 		$Query->Conditions = new \Framework\Newnorth\DbEqualTo('`Id`', (int)$Id);
 
 		return $this->_Find($Query);
 	}
-
+/*
 	public function FindUnsolvedByTestId($TestId) {
 		$Query = new \Framework\Newnorth\DbSelectQuery();
 
@@ -50,28 +30,42 @@ class TestFailureDataManager extends \Framework\Newnorth\DataManager {
 		$Query->Conditions->EqualTo('`TimeSolved`', 0);
 
 		return $this->_Find($Query);
-	}
+	} */
 
-	public function FindAllByTestId($TestId) {
+	public function FindAllByTestFailureId($TestFailureId, $SortColumn = '`PriorityLevel`', $SortOrder = DB_DESC) {
 		$Query = new \Framework\Newnorth\DbSelectQuery();
 
-		$Query->AddSource('TestFailure');
+		$Query->AddSource('TestFailure-State');
+
+		$Query->Conditions = new \Framework\Newnorth\DbAnd();
+
+		$Query->Conditions->EqualTo('`TestFailureId`', (int)$TestFailureId);
+
+		$Query->AddSort($SortColumn, $SortOrder);
+
+		return $this->_FindAll($Query);
+	}
+
+	public function FindAllByTestId($TestId, $SortColumn = '`PriorityLevel`', $SortOrder = DB_DESC) {
+		$Query = new \Framework\Newnorth\DbSelectQuery();
+
+		$Query->AddSource('TestFailure-State');
 
 		$Query->Conditions = new \Framework\Newnorth\DbAnd();
 
 		$Query->Conditions->EqualTo('`TestId`', (int)$TestId);
 
+		$Query->AddSort($SortColumn, $SortOrder);
+
 		return $this->_FindAll($Query);
 	}
 
-	public function FindAllActive($SortColumn = '`TimeFailed`', $SortOrder = DB_DESC) {
+	public function FindAll($SortColumn = '`PriorityLevel`', $SortOrder = DB_DESC) {
 		$Query = new \Framework\Newnorth\DbSelectQuery();
 
-		$Query->AddSource('TestFailure');
+		$Query->AddSource('TestFailure-State');
 
 		$Query->Conditions = new \Framework\Newnorth\DbAnd();
-
-		$Query->Conditions->EqualTo('`TimeSolved`', 0);
 
 		$Query->AddSort($SortColumn, $SortOrder);
 
